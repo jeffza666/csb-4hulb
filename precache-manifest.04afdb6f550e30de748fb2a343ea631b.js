63fdcd1adb2cb2f30910a09bc4afb4c73fb2b7b2
Code Sandbox <hello@codesandbox.io>
2022-01-12 10:39:29 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.04afdb6f550e30de748fb2a343ea631b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62ed2b9fdf3ce0c563693d8d9dbdd1d",
+    "url": "https://jeffza666.github.io/csb-4hulb/index.html"
+  },
+  {
+    "revision": "52d25d8ceb18c5b9d6c4",
+    "url": "https://jeffza666.github.io/csb-4hulb/static/css/main.401bce9b.chunk.css"
+  },
+  {
+    "revision": "8619c72f77aa1f17af03",
+    "url": "https://jeffza666.github.io/csb-4hulb/static/js/2.1fbb617a.chunk.js"
+  },
+  {
+    "revision": "a09bcd6464a11c77a5caeae57fde5da8",
+    "url": "https://jeffza666.github.io/csb-4hulb/static/js/2.1fbb617a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52d25d8ceb18c5b9d6c4",
+    "url": "https://jeffza666.github.io/csb-4hulb/static/js/main.1ef71920.chunk.js"
+  },
+  {
+    "revision": "8b4b58beed500c9898e0",
+    "url": "https://jeffza666.github.io/csb-4hulb/static/js/runtime-main.1a38c489.js"
+  }
+]);
